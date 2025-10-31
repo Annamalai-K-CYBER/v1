26f7702001d45a22c847728407b427ab5c3736b9
Annamalai.K <annamalai.kgm@gmail.com>
2025-10-31 07:56:23 +0530

    123

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -23,7 +23,7 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
-      const res = await fetch("https://csbssync.vercel.app/api/login", {
+      const res = await fetch("/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
