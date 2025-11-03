a37d1b1eb0155c8a0a812db99b7ca4fe4ae25eb7
Annamalai.K <annamalai.kgm@gmail.com>
2025-11-03 19:55:48 +0530

    123

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <script charset="UTF-8" src="//web.webpushs.com/js/push/bd9190069fcb1b2d3ec0d0b8222dceaa_1.js" async></script>
       <body>{children}</body>
       {/* <Analytics />          */}
     </html>
