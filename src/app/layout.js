717e0492b785da67485d8400ef6d4d46fb7b3fd8
Annamalai.K <annamalai.kgm@gmail.com>
2025-11-04 06:17:25 +0530

    123

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,9 +3,6 @@ import "./../styles/globals.css";
 export const metadata = {
   title: "CSBS SYNC",
   description: "Official class portal for CSBS Department",
-  icons: {
-    icon: "/favicon.jpg", // 👈 Your PNG favicon
-  },
 };
 
 export default function RootLayout({ children }) {
