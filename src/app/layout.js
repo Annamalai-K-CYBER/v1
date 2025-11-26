8667aa88d80a1b8c687de07afb62ce75b0539d55
Annamalai.K <annamalai.kgm@gmail.com>
2025-11-26 19:07:43 +0530

    123

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import "./../styles/globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 export const metadata = {
   title: "CSBS SYNC",
   description: "Official class portal for CSBS Department",
@@ -9,6 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Analytics/> 
         {children}
       </body>
     </html>
